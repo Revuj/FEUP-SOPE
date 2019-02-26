21c83344437aa623a58e3db1b755d207938fa8ad
Revuj <revuj112@gmail.com>
2019-02-26 22:36:23 +0000

    Added some explanation on the comments

--- Class2/p6a.c
+++ Class2/p6a.c
@@ -36,6 +36,8 @@ int main(int argc, char *argv[])
         printf("%-25s - %s; i-node - %ju;   size - %ju\n", direntp->d_name, str, (uintmax_t)direntp->d_ino, (uintmax_t)stat_buf.st_size);
         //symbolic links (pointer to file name) use less space than original file or hard link: this can only be verified using lstat instead of stat, because the first gives us info about symbolic links
         //file and its hard link have same i-node, the same cant be said for the symbolic link
+        //with the symbolic link file, after we erase (or name change) the original file, the symbolic link file cannot be found anymore because its nothing but a pointer to the name of the original file
+        //on the other hand, after the original file is deletes, the hard link file keeps its content
     }
     closedir(dirp);
     exit(0);
